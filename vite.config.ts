0989e2199f27c84d0280c241c52bf9589d49fae3
Vili Raunola <raunolavili@gmail.com>
2026-01-13 00:59:50 +0200

    fix on build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ mode }) => ({
+    base: '/CarWarmingTimer/',
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 }))
